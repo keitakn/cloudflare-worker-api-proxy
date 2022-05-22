42d88454f7504f0c420922b240b76eec9958d85f
keitakn <keita.koga.work@gmail.com>
2022-05-22 13:50:18 +0900

    :art: #2 itty-routerを使って必要なURLだけを許可出来るようなルーターを追加

---
+++ src/router.ts
@@ -0,0 +1,7 @@
+import { Router } from 'itty-router';
+import {handleCatImageValidation, handleNotFound} from "./handler";
+
+export const router = Router();
+
+router.post('/cat-images/validation-results', handleCatImageValidation);
+router.all('*', handleNotFound);
