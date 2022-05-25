cab7acd5d6d59a3ba2b7af992c940a9097c0dd3b
keitakn <keita.koga.work@gmail.com>
2022-05-25 17:18:35 +0900

    :sparkles: #9 ねこ画像をrandomで取得するAPIへのProxy機能を追加

---
+++ src/api/fetchLgtmImages.ts
@@ -0,0 +1,74 @@
+import { createFailureResult, createSuccessResult, Result } from '../result';
+
+import { JwtAccessToken } from './issueAccessToken';
+
+type LgtmImage = { id: number; url: string };
+
+type LgtmImages = LgtmImage[];
+
+type FetchLgtmImagesRequest = {
+  apiUrl: string;
+  accessToken: JwtAccessToken;
+};
+
+type SuccessResponse = {
+  lgtmImages: LgtmImage[];
+  xRequestId?: string;
+  xLambdaRequestId?: string;
+};
+
+type FailureResponse = {
+  error: Error;
+  xRequestId?: string;
+  xLambdaRequestId?: string;
+};
+
+export const fetchLgtmImagesInRandom = async (
+  request: FetchLgtmImagesRequest,
+): Promise<Result<SuccessResponse, FailureResponse>> => {
+  const options: RequestInit = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${request.accessToken}`,
+    },
+  };
+
+  const response = await fetch(`${request.apiUrl}/lgtm-images`, options);
+  if (!response.ok) {
+    const failureResponse: FailureResponse = {
+      error: new Error('failed to fetchLgtmImagesInRandom'),
+    };
+
+    if (response.headers.get('x-request-id')) {
+      failureResponse.xRequestId = response.headers.get(
+        'x-request-id',
+      ) as string;
+    }
+
+    if (response.headers.get('x-lambda-request-id')) {
+      failureResponse.xLambdaRequestId = response.headers.get(
+        'x-lambda-request-id',
+      ) as string;
+    }
+
+    return createFailureResult<FailureResponse>(failureResponse);
+  }
+
+  const responseBody = (await response.json()) as LgtmImages;
+
+  const successResponse: SuccessResponse = {
+    lgtmImages: responseBody,
+  };
+
+  if (response.headers.get('x-request-id')) {
+    successResponse.xRequestId = response.headers.get('x-request-id') as string;
+  }
+
+  if (response.headers.get('x-lambda-request-id')) {
+    successResponse.xLambdaRequestId = response.headers.get(
+      'x-lambda-request-id',
+    ) as string;
+  }
+
+  return createSuccessResult<SuccessResponse>(successResponse);
+};
--- src/global.d.ts
+++ src/global.d.ts
@@ -3,4 +3,5 @@
 declare const COGNITO_CLIENT_ID: string;
 declare const COGNITO_CLIENT_SECRET: string;
 declare const COGNITO_TOKEN_ENDPOINT: string;
+declare const LGTMEOW_API_URL: string;
 declare const IMAGE_RECOGNITION_API_URL: string;
