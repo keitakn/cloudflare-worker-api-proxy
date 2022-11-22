1831529f93e0f152a16e970706c37fa05fa0c9db
keitakn <keita.koga.work@gmail.com>
2022-11-23 00:28:01 +0900

    :fire: #15 honoに変更した事で不要になったファイルを削除

--- src/global.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable init-declarations */
-
-declare const COGNITO_CLIENT_ID: string;
-declare const COGNITO_CLIENT_SECRET: string;
-declare const COGNITO_TOKEN_ENDPOINT: string;
-declare const LGTMEOW_API_URL: string;
-declare const IMAGE_RECOGNITION_API_URL: string;
